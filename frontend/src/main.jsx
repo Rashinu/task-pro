5f8acdc4a92e511dde5fc8cf1bb533d5c3820894
Rüsgar <50423527+Rashinu@users.noreply.github.com>
2026-06-12 17:58:51 +0300

    Configure frontend for GitHub Pages deployment

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -10,7 +10,7 @@ import { store } from "./redux/store";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename="/task-pro">
         <App />
         <Toaster position="top-right" />
       </BrowserRouter>
